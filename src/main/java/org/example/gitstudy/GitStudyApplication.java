6d1d3e60b77a194a9d344673ea90da91d79cd001
BaeWonJin <try3982@naver.com>
2024-07-17 13:53:33 +0900

    new line

--- src/main/java/org/example/gitstudy/GitStudyApplication.java
+++ src/main/java/org/example/gitstudy/GitStudyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitStudyApplication {
 
     public static void main(String[] args) {
+        System.out.println("Hello Git-Study");
         SpringApplication.run(GitStudyApplication.class, args);
     }
 
